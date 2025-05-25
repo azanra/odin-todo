4d8533220ddd0f58e7fa827c9876f78320b37c94
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-25 10:09:59 +0700

    Rename projectList and store id of todo instead

--- src/script/model/project.js
+++ src/script/model/project.js
@@ -1,7 +1,54 @@
-export default function Project() {
-  this.project = [];
+import { localData } from "./localStorage.js";
+
+const todoList = localData.todoList.data;
+
+export default function Project(name, id) {
+  this.list = [];
+  this.name = name;
+  this.id = id;
+  this.type = "list";
 }
 
-Project.prototype.addProjectList = function (projectList) {
-  this.project = [...this.project, projectList];
+Project.prototype.addTodo = function (todo) {
+  this.list = [...this.list, todo];
+};
+
+Project.prototype.setChecklist = function (id) {
+  this.list.map((item) => {
+    if (item === id) {
+      todoList[id].setChecked();
+    }
+  });
+};
+
+Project.prototype.setIsOpen = function (id) {
+  this.list.map((item) => {
+    if (item === id) {
+      todoList[id].setIsOpen();
+    }
+  });
+};
+
+Project.prototype.getDueDate = function (id) {
+  let dueDate;
+  this.list.map((item) => {
+    if (item === id) {
+      dueDate = todoList[id].getDueDate();
+    }
+  });
+  return dueDate;
+};
+
+Project.prototype.setAttribute = function (id, attribute, newValue) {
+  this.list.map((item) => {
+    if (item === id) {
+      todoList[id].setAttribute(attribute, newValue);
+    }
+  });
+};
+
+Project.prototype.deleteTodo = function (id) {
+  this.list = this.list.filter((item) => {
+    return item !== id;
+  });
 };
