780143a891dce6aabf9c1539782a37c69a50d3c3
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-30 08:37:39 +0700

    Create method that create view for project card

---
+++ src/script/view/projectCard.js
@@ -0,0 +1,14 @@
+import { attribute } from "./attribute.js";
+import { element } from "./createElement.js";
+
+export const projectCard = (function () {
+  const createCard = (project) => {
+    const projectCard = attribute.projectCard;
+    for (const key in projectCard) {
+      const attr = element.setUniqueId(projectCard[key], project.id);
+      element.createDom(attr, project);
+    }
+  };
+
+  return { createCard };
+})();
