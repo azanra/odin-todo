f2c842213c79e9e6e43ee7c4aac69d6dfadd02fb
azan-personal <112457939+azanra@users.noreply.github.com>
2025-05-30 08:38:04 +0700

    Add attribute object for project card

--- src/script/view/attribute.js
+++ src/script/view/attribute.js
@@ -8,6 +8,7 @@ export const attribute = {
       },
       uniqueParent: false,
       uniqueElement: true,
+      uniqueText: false,
     },
     headerContainer: {
       parentElement: "#projectContainer",
@@ -17,6 +18,39 @@ export const attribute = {
       },
       uniqueParent: true,
       uniqueElement: true,
+      uniqueText: false,
+    },
+    headerProject: {
+      parentElement: "#headerContainer",
+      newElement: "h1",
+      elementAttribute: {
+        class: "headerProject",
+      },
+      uniqueParent: true,
+      uniqueElement: false,
+      uniqueText: true,
+    },
+    detailContainer: {
+      parentElement: "#projectContainer",
+      newElement: "div",
+      elementAttribute: {
+        class: "detailContainer",
+      },
+      uniqueParent: true,
+      uniqueElement: true,
+      uniqueText: false,
+    },
+    detailBtn: {
+      parentElement: "#detailContainer",
+      newElement: "button",
+      elementAttribute: {
+        type: "button",
+        class: "detailBtn",
+      },
+      textContent: "Detail",
+      uniqueParent: true,
+      uniqueElement: true,
+      uniqueText: false,
     },
   },
 };
