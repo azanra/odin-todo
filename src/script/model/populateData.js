1f8bdc9b2ae3ed60e56a48f7e9e5879d192a5179
azan-personal <112457939+azanra@users.noreply.github.com>
2025-06-01 11:23:32 +0700

    Add method to create project object from data

--- src/script/model/populateData.js
+++ src/script/model/populateData.js
@@ -1,13 +1,21 @@
 import { localData } from "./localStorage.js";
 import Todo from "./todo.js";
+import Project from "./project.js";
 
 export const populateData = (function () {
-  const todo = (data) => {
+  const todoData = (data) => {
     for (const key in data) {
       const todo = new Todo(data[key]);
       localData.todoList.data[todo.id] = todo;
     }
     return localData.todoList.data;
   };
-  return { todo };
+  const projectData = (data) => {
+    for (const key in data) {
+      const project = new Project(data[key]);
+      localData.projectList.data[project.id] = project;
+    }
+    return localData.projectList.data;
+  };
+  return { todoData, projectData };
 })();
