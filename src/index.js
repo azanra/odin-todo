c14191836da17b650c38740fde7282fe89bb2851
azan-personal <112457939+azanra@users.noreply.github.com>
2025-06-12 06:21:53 +0700

    Add another module to hold data to avoid circular dependency

--- src/index.js
+++ src/index.js
@@ -1,21 +1,11 @@
+import { projectData, todoData } from "./mainData.js";
 import { controller } from "./script/controller/controller.js";
 import { localData } from "./script/model/localStorage.js";
-import { populateData } from "./script/model/populateData.js";
-import { attribute } from "./script/view/attribute.js";
 import { projectCard } from "./script/view/projectCard.js";
-import { todoCard } from "./script/view/todoCard.js";
 import { todoSelectProject } from "./script/view/todoSelectProject.js";
 import "./style.css";
 
-export const todoData = populateData.checkData("todo");
-console.log(todoData);
-
-export const projectData = populateData.checkData("project");
-console.log(projectData);
-
 projectCard.renderCard(projectData);
-// todoCard.renderCard(projectData, todoData, attribute.todoCard);
-// todoCard.renderCard(projectData, todoData, attribute.todoDetail);
 todoSelectProject.renderOption(projectData);
 controller.listenToEvent();
 
---
+++ src/mainData.js
@@ -0,0 +1,4 @@
+import { populateData } from "./script/model/populateData";
+
+export const todoData = populateData.checkData("todo");
+export const projectData = populateData.checkData("project");
